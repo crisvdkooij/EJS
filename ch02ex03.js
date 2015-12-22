506e22a68b7aa951ae4373cac4c0e56512156e41
crisvdkooij <cristopher@cristopher.nl>
2015-12-22 23:10:28 +0100

    Update ch02ex03.js

--- ch02ex03.js
+++ ch02ex03.js
@@ -20,3 +20,22 @@ for (i = 0; i <= size*size; i++) {
   }
 }
 console.log(grid);
+
+//Solution given by book
+/*
+var size = 8;
+
+var board = "";
+
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0)
+      board += " ";
+    else
+      board += "#";
+  }
+  board += "\n";
+}
+
+console.log(board);
+*/
