07e16735b16707e02620152e9672827b3d070c6a
crisvdkooij <cristopher@cristopher.nl>
2015-12-22 22:59:18 +0100

    Create ch02ex01.js

---
+++ ch02ex01.js
@@ -0,0 +1,12 @@
+//Solution creates strings through the join() function. This way you can also control the number of hashes the tree starts of with.
+for (i = 2; i < 9; i++) {
+  console.log(Array(i).join("#"));
+}
+
+//Alternate solution adding hashes to a string
+/*
+var hashes = ""
+for (i = 0; i < 7; i++) {
+  console.log(hashes += "#");
+}
+*/
