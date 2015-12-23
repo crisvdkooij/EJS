b7eabe9fa8ddb18cd952135c9bb3abd15c8d826c
crisvdkooij <cristopher@cristopher.nl>
2015-12-23 18:03:18 +0100

    Create ch03ex03.js

---
+++ ch03ex03.js
@@ -0,0 +1,30 @@
+// Bean counting
+
+function countBs(str) {
+  var charCount = 0;
+  for(i = 0; i < str.length; i++)
+    if(str.charAt(i) === "B")
+      charCount++;
+  return(charCount);
+}
+
+/* Solution following book instructions
+function countChar(str,toFind) {
+  var charCount = 0;
+  for(i = 0; i < str.length; i++)
+    if(str.charAt(i) === toFind)
+      charCount++;
+  return(charCount);
+}
+*/
+
+// Alternate solution with regex
+function countChar(str,toFind) {
+  var re = new RegExp(toFind,'g');
+  return str.match(re).length;
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
